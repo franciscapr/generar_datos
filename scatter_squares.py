c172b4facddfc11d9559e6b0d0cb61655cdf635a
Francisca <francisca.paredes.pr@gmail.com>
2025-02-12 10:11:47 -0300

    calcular datos

---
+++ scatter_squares.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+x_values = range(1, 1001)
+y_values = [x ** 2 for x in x_values]
+
+plt.style.use('Solarize_Light2')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, s=10)
+
+# Establece el tìtulo del gràfico y las etiquetas de los ejes.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Square of Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Establece el tamaño de las etiquetas de los puntos de los ejes.
+ax.tick_params(labelsize=14)
+
+ax.axis([0, 1100, 0, 1_100_000])
+
+plt.show()
