ef9918ecae3ec4f2f51661081f44d088d7b2d12f
Francisca <francisca.paredes.pr@gmail.com>
2025-02-12 09:33:35 -0300

    mejorando el trazado

--- mpl_squares.py
+++ mpl_squares.py
@@ -2,6 +2,14 @@ import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# Establece el tìtulo del gràfico y las etiquetas de los ejes.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Square of Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Establece el tamaño de las etiquetas de los puntos de los ejes.
+ax.tick_params(labelsize=14)
 
 plt.show()
