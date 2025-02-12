479817a01921a34a82adfd6f02c201ccf80a2302
Francisca <francisca.paredes.pr@gmail.com>
2025-02-12 15:30:32 -0300

    multiples caminatas

--- rw_visual.py
+++ rw_visual.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-# Crea una caminata aleatoria.
-rw = RandomWalk()
-rw.fill_walk()
+# Sigue generando caminatas nuevas mientras el programa estè activo.
+while True:
+    # Crea una caminata aleatoria.
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Traza los puntos de la caminata.
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal')
-plt.show()
+    # Traza los puntos de la caminata.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    ax.set_aspect('equal')
+    plt.show()
+
+    keep_running = input('Make another walk? (y/n): ')
+    if keep_running == 'n':
+        break
