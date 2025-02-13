75987b1f38481d67fe8e91d34b1c0d5ed8006772
Francisca <francisca.paredes.pr@gmail.com>
2025-02-12 21:09:43 -0300

    edgecolors

--- rw_visual.py
+++ rw_visual.py
@@ -10,7 +10,9 @@ while True:
     # Traza los puntos de la caminata.
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+
     ax.set_aspect('equal')
     plt.show()
 
