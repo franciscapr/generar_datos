f6946cdf9eabc1bb39212e9a9d94c102b25651f5
Francisca <francisca.paredes.pr@gmail.com>
2025-03-06 12:33:30 -0300

    analizar los resultados

--- die_visual.py
+++ die_visual.py
@@ -6,9 +6,17 @@ die = Die()    # Creamos una instancia de Die de seis lados por defecto.
 # Hace algunas tiradas y guarda los resultados en un lista.
 results = []    # Creamos una lista vacia.
 
-for roll_num in range(100):    # Tiramos el dado 100 veces
+for roll_num in range(1000):    # Tiramos el dado 100 veces
     result = die.roll()
     results.append(result)    # Almacenamos los resultados en la lista results
 
 # Imprimimos la lista
-print(results)
+# print(results)
+
+# Analiza los resultados.
+frequencies = []
+poss_results = range(1, die.num_sides+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+print(frequencies)
